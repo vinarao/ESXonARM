414e6b1d2e6406924b0b55b22fa76f5e6db72505
Vinay Rao <vinay.c.rao@oracle.com>
2021-09-30 22:19:32 -0700

    Update

--- modules/vcn/main.tf
+++ modules/vcn/main.tf
@@ -3,7 +3,7 @@
 resource "oci_core_virtual_network" "vcn" {
   cidr_block     = "10.0.0.0/16"
   compartment_id = "${var.compartment_ocid}"
-  display_name   = "vmware-vcn"
+  display_name   = "arm-vcn"
   dns_label      = "ocvs"
 }
 
